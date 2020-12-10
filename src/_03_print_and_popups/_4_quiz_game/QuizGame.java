b131598aeb793918c890109f148542bda6e32857
root <root@1cca39affa78>
2020-12-10 20:28:20 +0000

    dragonfight

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,31 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score =0; 
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+				String answer = JOptionPane.showInputDialog("what state are we in");
 				// 3.  Use an if statement to check if their answer is correct
-				
+				if(answer.equalsIgnoreCase("California")) {
+					JOptionPane.showMessageDialog(null , "You are correct");
+				score = score +1;
+	}else {
+		JOptionPane.showMessageDialog(null , "Incorrect! the answer is California");
+	}
 				// 4.  if the user's answer was correct, add one to their score 
+				
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
 		
 		
 		// After all the questions have been asked, tell the user their final score 
-		
+		JOptionPane.showMessageDialog(null , "you answered" + score + "question(s) correctly.");
 	}
 }
